a1f2cb4928e3f7978b7e7ca3dfece6233d070500
Thomas Dy <thatsmydoing@gmail.com>
2017-12-28 16:30:09 +0900

    Move init to HTML

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="util.ts" />
-/// <reference path="game.ts" />
-
-util.loadBase();
-let container: HTMLElement = document.querySelector('#container');
-let controller = new game.MainController(container, 'levels.json');
-controller.start();
